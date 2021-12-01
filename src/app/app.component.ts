f7a3fa549f4cbdf42544962ef3dacce17c3797df
teresanancy <teresa.nancy@arisglobal.com>
2021-12-01 19:29:01 +0530

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app';
+  name="Teresa";
+  getName(){
+    return this.name;
+  }
+  arr=[1,2,3]
+}
